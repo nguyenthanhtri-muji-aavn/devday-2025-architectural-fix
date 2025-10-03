e4e2969e3763e1025ff881db581e4ebb3f85a0b2
Nguyen Thanh Tri <nttri02@axonactive.com>
2025-10-03 18:02:27 +0700

    config CORS

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['via.placeholder.com'],
-    },
-    output: 'standalone',
-    experimental: {
-        appDir: true
-    }
+  async rewrites() {
+    return [
+      {
+        source: '/api/external/:path*',
+        destination:
+          'https://devday-aavn-d5284e914439.herokuapp.com/api/:path*',
+      },
+    ];
+  },
+  images: {
+    domains: ['via.placeholder.com'],
+  },
+  output: 'standalone',
+  experimental: {
+    appDir: true,
+  },
 };
 
 export default nextConfig;
